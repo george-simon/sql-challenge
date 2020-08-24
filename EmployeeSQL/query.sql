294c48c160b3f3048eb3bed3516158c8ec0ea185
george-simon <george-simon@users.noreply.github.com>
2020-08-23 19:39:07 -0700

    query addition

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -60,3 +60,7 @@ WHERE dept_name LIKE 'Develop%'
 
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "Frequency of last_name"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency of last_name" DESC;
